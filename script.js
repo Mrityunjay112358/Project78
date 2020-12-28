c3dacda03416a357e43382b494a3f25ad9930822
Mrityunjay112358 <73526816+Mrityunjay112358@users.noreply.github.com>
2020-12-28 09:58:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 var arrayimg = [
-    "cover.jpg",
     "mother.jpg",
     "brother.png",
     "brother.jpg",
-    "father.jpg",
-   ]
-var name = [
-    Family,
-    Mother,
-    Me,
-    Brother,
-    Father
-]
+    "father1.png"
+   ];
+var array_name = [
+    "Mother",
+    "Me",
+    "Brother",
+    "Father"
+];
+var i = 0;
+function nextslide()
+{
 
-function nextslide(){
-    document.getElementById("name1").src=name[i];
-    i++;
+    
+    document.getElementById("name1").innerHTML=array_name[i];
+    
+
     document.getElementById("img1").src=arrayimg[i];
     i++;
-   }
+    }
+
+   
